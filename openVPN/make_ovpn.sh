4f38bc7de13ec8f59f627aeb97898e4becfe9ba4
Pavel Zavodyan <pavel@pavel.zavodyan@mail.ru>
2024-08-17 14:31:59 +0000

    fix make_ovpn

--- openVPN/make_ovpn.sh
+++ openVPN/make_ovpn.sh
@@ -4,13 +4,6 @@ path_to_openvpn_dir=~/projectVPN/openVPN
 max=-1
 cur=1
 
-if [ "$mode" == "along" ];
-then
-       	max=8
-else
-       	max=7
-fi
-
 echo -ne "\nВведите 'along', если сервис openvpn будет на машине с центром сертификации, иначе введите 'none': "
 
 read mode
@@ -26,6 +19,13 @@ do
 	fi		
 done
 
+if [ "$mode" == "along" ];
+then
+       	max=9
+else
+       	max=7
+fi
+
 function checkwork {
 	
 	code=$?
@@ -40,7 +40,7 @@ function checkwork {
 }
 
 
-echo -e "\e[0;34m\nЭтап $cur/$max. Установка openVPN.\e[0m"
+echo -e "\e[0;35m\nЭтап $cur/$max. Установка openVPN.\e[0m"
 sudo apt-get update -y > /dev/null && sudo apt-get install openvpn -y > /dev/null
 checkwork $cur
 cur=$(( cur + 1 ))
@@ -48,7 +48,7 @@ cur=$(( cur + 1 ))
 
 if [ "$mode" != "along" ];
 then
-	echo -e "\e[0;34m\nЭтап $cur/$max. Установка и настройка easy-rsa.\e[0m"
+	echo -e "\e[0;35m\nЭтап $cur/$max. Установка и настройка easy-rsa.\e[0m"
 	sudo apt-get update -y > /dev/null && sudo apt-get install easy-rsa -y > /dev/null && \
 	mkdir -p ~/easy-rsa && ln -sf /usr/share/easy-rsa/* ~/easy-rsa/ && cd ~/easy-rsa  && ./easyrsa init-pki && \
 	cp $path_to_openvpn_dir/vars . &&  echo -e "\e[0;32mЭтап $1 прошел успешно!\n\e[0m" || echo "\e[0;31mOшибка на этапе $cur!\e[0m"
@@ -57,45 +57,45 @@ else
 	cd ~/easy-rsa
 fi
 
-echo -e "\e[0;34mЭтап $cur/$max. Создание запроса на сертификат и ключ для сервера.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$max. Создание запроса на сертификат и ключ для сервера.\e[0m"
 ./easyrsa --batch gen-req server nopass 
 checkwork $cur
 cur=$(( cur + 1 ))
 
-echo -e "\e[0;34mЭтап $cur/$max Генерация tls-ключа для дополнительной безопасности.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$max Генерация tls-ключа для дополнительной безопасности.\e[0m"
 openvpn --genkey --secret ta.key
 checkwork $cur
 cur=$(( cur + 1 ))
 
 if [ "$mode" == "along" ];
 then
-	echo -e "\e[0;34mЭтап $cur/$max. Подпись сертификата удостоверяющим центром.
+	echo -e "\e[0;35mЭтап $cur/$max. Подпись сертификата удостоверяющим центром.
 	Здесь потребуется ввести пароль, заданный при генерации корневого сертефиката СA.\e[0m"
 	./easyrsa --batch sign-req server server
 	checkwork $cur
 	cur=$(( cur + 1 ))
 
-	echo -e "\e[0;34mЭтап $cur/$max. Копирование сертификата CA и сертификата сервера в /etc/openvpn/server/\e[0m"
+	echo -e "\e[0;35mЭтап $cur/$max. Копирование сертификата CA и сертификата сервера в /etc/openvpn/server/\e[0m"
 	sudo cp ./pki/ca.crt /etc/openvpn/server/ && \
 	sudo cp ./pki/issued/server.crt /etc/openvpn/server/
 	checkwork $cur
 	cur=$(( cur + 1 ))
 fi
 
-echo -e "\e[0;34mЭтап $cur/$max. Копирование секретного ключа сервера и дополнительного ключа ta.key в /etc/openvpn/server.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$max. Копирование секретного ключа сервера и дополнительного ключа ta.key в /etc/openvpn/server.\e[0m"
 sudo cp ./pki/private/server.key /etc/openvpn/server && \
 	sudo cp ./ta.key /etc/openvpn/server/
 checkwork $cur
 cur=$(( cur + 1 ))
 
-echo -e "\e[0;34mЭтап $cur/$cur Настройка openVPN и маршрутизации.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$cur Настройка openVPN и маршрутизации.\e[0m"
 sudo cp $path_to_openvpn_dir/server.conf /etc/openvpn/server && \
 	sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf && \
 	sudo sysctl -p
 checkwork $cur
 cur=$(( cur + 1 ))
 
-echo -e "\e[0;34mЭтап $cur/$max. Настройка iptables.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$max. Настройка iptables.\e[0m"
 sudo apt-get install iptables -y > /dev/null && \
 	int=$(ip a | grep -e "eth.:" | awk -F: '{print $2}' | sed 's/ //') && \
 	proto=$(sudo cat /etc/openvpn/server/server.conf | grep ^proto | awk '{print $2}') && \
@@ -105,7 +105,7 @@ sudo apt-get install iptables -y > /dev/null && \
 checkwork $cur
 cur=$(( cur + 1 ))
 
-echo -e "\e[0;34mЭтап $cur/$max Запуск openVPN.\e[0m"
+echo -e "\e[0;35mЭтап $cur/$max Запуск openVPN.\e[0m"
 sudo systemctl -f enable openvpn-server@server.service && \
 	sudo systemctl start openvpn-server@server.service 
 checkwork $cur
@@ -124,4 +124,6 @@ then
 else
 	echo -e "\nПередайте запрос на полечение сертификата в Удостоверяющий Центр (CA)."
         echo -e "Запросите у удостоверяющего центра ca.crt и server.crt."
+	echo -e "\e[0;33mПоложите полученные файлы в /etc/openvpn/server/ 
+	Перезапустите сервис openvpn: sudo systemctl restart openvpn-server@server.service"
 fi
